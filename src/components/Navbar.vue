6d2c10bdad9171e9bf8a5339034b1489d8d8f55b
vinayBomma <vinaysb2000@gmail.com>
2019-06-14 18:18:08 +0530

    Initial Commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,33 @@
+<template>
+  <nav>
+    <v-toolbar flat app>
+      <v-toolbar-side-icon v-on:click="drawer = !drawer"></v-toolbar-side-icon>
+      <!-- <v-toolbar-title>Test</v-toolbar-title> -->
+      <v-spacer></v-spacer>
+    </v-toolbar>
+
+    <v-navigation-drawer v-model="drawer" app>
+      <v-list>
+        <v-list-tile v-for="link in links" v-bind:key="link.text" router v-bind:to="link.route">
+          <v-list-tile-action>
+            <v-icon>{{ link.icon }}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title class="grey--text">{{ link.text }}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+  </nav>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+        drawer: false
+    };
+  }
+};
+</script>
+
